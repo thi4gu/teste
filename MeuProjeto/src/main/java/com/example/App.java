0b158079fbcc4307fe3f8c02174cb6ed5a57ccad
Thiago <thi4gu@gmail.com>
2024-02-28 17:50:34 -0300

    Adicionado comentário como pedido por fulano

--- MeuProjeto/src/main/java/com/example/App.java
+++ MeuProjeto/src/main/java/com/example/App.java
@@ -12,6 +12,7 @@ public class App
     public static void main(String[] args )
     {
         Centro centro = new Centro();
+        // Comentario bla bla bla
         centro.setNome("Centro Novo de Novo 3.2");
         System.out.println( centro.getNome() );
     }
