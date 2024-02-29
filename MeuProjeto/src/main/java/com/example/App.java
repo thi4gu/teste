9add0ab107714de67559720c4380e9ea19c06b2b
Thiago <thi4gu@gmail.com>
2024-02-29 10:47:21 -0300

    Comentário Eclipse

--- MeuProjeto/src/main/java/com/example/App.java
+++ MeuProjeto/src/main/java/com/example/App.java
@@ -12,7 +12,7 @@ public class App
     public static void main(String[] args )
     {
         Centro centro = new Centro();
-        // Comentario bla bla bla
+        // Comentario Eclipse
         centro.setNome("Centro Novo de Novo 3.6 Intellij");
         centro.setNome("Centro Novo de Novo 3.6 Eclipse");
         System.out.println( centro.getNome() );
