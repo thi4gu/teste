ed874bdd1c8c9a64fa7a313ff3fff2de0eb21a9a
Thiago <thi4gu@gmail.com>
2024-02-29 10:34:56 -0300

    Versão 3.6 Eclipse

--- MeuProjeto/src/main/java/com/example/App.java
+++ MeuProjeto/src/main/java/com/example/App.java
@@ -14,7 +14,7 @@ public class App
         Centro centro = new Centro();
         // Comentario bla bla bla
         centro.setNome("Centro Novo de Novo 3.5 Intellij");
-        centro.setNome("Centro Novo de Novo 3.5 Eclipse");
+        centro.setNome("Centro Novo de Novo 3.6 Eclipse");
         System.out.println( centro.getNome() );
     }
 
