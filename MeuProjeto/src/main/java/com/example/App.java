b5305f5006f0e47a8528c4065f5fe6c80b3d73e4
Thiago <thi4gu@gmail.com>
2024-02-29 07:34:48 -0300

    Commit eclipse
    Change-Id: Ie49c5fc8f9fa0df433f74c48cd838808298888a7

--- MeuProjeto/src/main/java/com/example/App.java
+++ MeuProjeto/src/main/java/com/example/App.java
@@ -12,7 +12,7 @@ public class App
     public static void main(String[] args )
     {
         Centro centro = new Centro();
-        centro.setNome("Centro Novo de Novo 3.4"); 
+        centro.setNome("Centro Novo de Novo 3.4 Eclipse"); 
         System.out.println( "Nome: " + centro.getNome() );
     }
 
---
+++ MeuProjeto/src/main/java/com/example/Centrable.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public interface Centrable {
+
+	String getNome();
+
+	void setNome(String nome);
+
+}
